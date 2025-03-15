8d2f1672a7fa4f715585ac9f0950c3c9f8ac72aa
Pol Pinol Castuera <pol.pinol@mongodb.com>
2025-03-15 20:09:33 +0100

    Initial structure

---
+++ src/main/java/io/autoinvestor/market_feeling/MarketFeelingApplication.java
@@ -0,0 +1,13 @@
+package io.autoinvestor.market_feeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketFeelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketFeelingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/autoinvestor/market_feeling/MarketFeelingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.autoinvestor.market_feeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketFeelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
