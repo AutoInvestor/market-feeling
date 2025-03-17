94ddd5c408eb19c72acb13ecdcef89f380db86b1
Pol Pinol Castuera <pol.pinol@mongodb.com>
2025-03-17 19:39:34 +0100

    Add initial structure and pipelines

--- src/test/java/io/autoinvestor/MarketFeelingApplicationTests.java
+++ src/test/java/io/autoinvestor/MarketFeelingApplicationTests.java
@@ -1,4 +1,4 @@
-package io.autoinvestor.market_feeling;
+package io.autoinvestor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MarketFeelingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
