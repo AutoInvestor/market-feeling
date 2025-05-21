07a9242803fbde94d610bcb06e7d589e6cb292ff
Pol Pinol Castuera <pol.pinol@mongodb.com>
2025-05-21 20:29:37 +0200

    Change to datetime

--- packages/stock_api/infrastructure/listeners/pubsub_event.py
+++ packages/stock_api/infrastructure/listeners/pubsub_event.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class PubSubEventDTO:
     event_id: str
     aggregate_id: str
-    occurred_at: str
+    occurred_at: datetime
     version: int
     type: str
     payload: dict
